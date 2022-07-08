b9cc4a2820ecccccd041501f90e892c760383627
kingananas20 <105493588+kingananas20@users.noreply.github.com>
2022-07-08 18:52:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,26 @@ ascii_banner = pyfiglet.figlet_format("Tools")
 print(f"{Fore.RED}{ascii_banner} created by kingananas2.0 with tools from icode4u")
 print(f"""
 What do you wanna use:
-{Fore.WHITE}[ {Fore.YELLOW}1 {Fore.WHITE}] Minecraft Server Creater                          credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}2 {Fore.WHITE}] SpamBot                                           credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}3 {Fore.WHITE}] YouTube Downloader                                credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}4 {Fore.WHITE}] Py to exe Converter with icon                     credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}5 {Fore.WHITE}] Py to exe Converter without a icon                credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}6 {Fore.WHITE}] Image Downloader                                  credits icode4u   |   improved by kingananas2.0
-{Fore.WHITE}[ {Fore.YELLOW}7 {Fore.WHITE}] Url Checker                                       credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}1 {Fore.WHITE}] SpamBot                                           credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}2 {Fore.WHITE}] YouTube Downloader                                credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}3 {Fore.WHITE}] Py to exe Converter with icon                     credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}4 {Fore.WHITE}] Py to exe Converter without a icon                credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}5 {Fore.WHITE}] Image Downloader                                  credits icode4u   |   improved by kingananas2.0
+{Fore.WHITE}[ {Fore.YELLOW}6 {Fore.WHITE}] Url Checker                                       credits icode4u   |   improved by kingananas2.0
 """)
 start = int(input(f"{Fore.YELLOW}> "))
 
 if start == 1:
-    subprocess.call("autoserver.py", shell = True)
-elif start == 2:
     subprocess.call("spambot.py", shell = True)
-elif start == 3:
+elif start == 2:
     subprocess.call("youtubedownloader.py", shell = True)
-elif start == 4:
+elif start == 3:
     subprocess.call("pytoexewithico.py", shell = True)
-elif start == 5:
+elif start == 4:
     subprocess.call("pytoexe.py", shell = True)
-elif start == 6:
+elif start == 5:
     subprocess.call("imagedownloader.py", shell = True)
-elif start == 7:
+elif start == 6:
     subprocess.call("urlchecker.py", shell = True)
 else:
     exit
